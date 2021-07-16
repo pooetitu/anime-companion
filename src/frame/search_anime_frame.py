1eed4154716343dfb8a938f1600847ba2c7c7e85
pooetitu <pooetitu@gmail.com>
2021-07-16 19:21:29 +0200

    Create base search anime tab

--- src/frame/search_anime_frame.py
+++ src/frame/search_anime_frame.py
@@ -1,4 +1,4 @@
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
 
 class SearchAnimeFrame(ttk.Frame):
@@ -6,7 +6,24 @@ class SearchAnimeFrame(ttk.Frame):
         super().__init__(master)
         self.master = master
         self.pack()
+        self.container = ttk.Frame(self)
+        self.textField = ttk.Entry(self.container)
+        self.searchButton = ttk.Button(self.container, text="Chercher")
+        self.selectFileButton = ttk.Button(self, text="Chercher par image")
         self.create_widgets()
 
     def create_widgets(self):
-        print()
+        self.searchButton.pack()
+        self.container.pack(side="top")
+        self.textField.pack(in_=self.container, side="left")
+        self.searchButton.pack(in_=self.container, side="left")
+        self.selectFileButton["command"] = self.open_file_selection
+        self.selectFileButton.pack()
+
+    def open_file_selection(self):
+        file = filedialog.askopenfile(parent=self, mode='rb', title='Choisissez une image à rechercher', filetypes=[
+            ("image", ".jpeg"),
+            ("image", ".png"),
+            ("image", ".jpg")])
+        if file:
+            print(file.read())
